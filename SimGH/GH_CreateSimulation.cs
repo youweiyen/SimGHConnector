66cd9496a9a10a2bd99e0db736ead1765693949c
yyen <yyen@kglakademi.dk>
2025-08-19 11:45:37 +0200

    seperate set environment node and create environment node

---
+++ SimGH/GH_CreateSimulation.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types.Transforms;
+using Rhino.Geometry;
+using SimScale.Sdk.Api;
+using SimScale.Sdk.Model;
+using Point = SimScale.Sdk.Model.Point;
+
+namespace SimGH
+{
+    public class GH_CreateSimulation : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GH_CreateSimulation class.
+        /// </summary>
+        public GH_CreateSimulation()
+          : base("CreateSim", "CS",
+              "Create Simulation Model",
+              "SimGH", "Set")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("ProjectID", "P", "ProjectID", GH_ParamAccess.item);
+            pManager.AddTextParameter("GeometryID", "G", "GeometryID", GH_ParamAccess.item);
+            pManager.AddGenericParameter("GeometryAPI", "GA", "Geometry API", GH_ParamAccess.item);
+            pManager.AddGenericParameter("SimulationAPI", "SA", "Simulation API", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string projectId = default;
+            string geometryIdText = default;
+            var geometryApi = new GeometriesApi();
+            var simulationApi = new SimulationsApi();
+
+
+            DA.GetData(0, ref projectId);
+            DA.GetData(1, ref geometryIdText);
+            DA.GetData(2, ref geometryApi);
+            DA.GetData(3, ref simulationApi);
+
+            Guid geometryId = new Guid(geometryIdText);
+
+            // Read geometry information and update with the deserialized model
+            var geometry = geometryApi.GetGeometry(projectId, geometryId);
+            geometryApi.UpdateGeometry(projectId, geometryId, geometry);
+
+
+            // Get geometry mappings
+            var materialEntity = getSingleEntityName(
+                geometryApi,
+                projectId,
+                geometryId,
+                values: new List<string> { "block1" }
+            );
+            var bc1Entity = getSingleEntityName(
+                geometryApi,
+                projectId,
+                geometryId,
+                values: new List<string> { "block2" }
+            );
+            var bc2Entity = getSingleEntityName(
+                geometryApi,
+                projectId,
+                geometryId,
+                values: new List<string> { "block3" }
+            );
+            var bc3Entity = getSingleEntityName(
+                geometryApi,
+                projectId,
+                geometryId,
+                values: new List<string> { "block4" }
+            );
+            var bc4Entity = getSingleEntityName(
+                geometryApi,
+                projectId,
+                geometryId,
+                values: new List<string> { "Wall 1" }
+            );
+
+            // Create geeometry primitive to use as probe point in simulation
+            var geometryPrimitivePoint = new Point(
+                name: "Point 1",
+                center: new DimensionalVectorLength(
+                    value: new DecimalVector(
+                        x: 0,
+                        y: 0,
+                        z: -0
+                    ),
+                    unit: DimensionalVectorLength.UnitEnum.M
+                )
+            );
+            var geometryPrimitiveId = simulationApi.CreateGeometryPrimitive(
+                projectId,
+                geometryPrimitivePoint
+            ).GeometryPrimitiveId;
+            Console.WriteLine("geometryPrimitiveUuid: " + geometryPrimitiveId);
+
+
+            // Create simulation model
+            var simulationModel = new ConvectiveHeatTransfer(
+                isCompressible: false,
+                turbulenceModel: ConvectiveHeatTransfer.TurbulenceModelEnum.KOMEGASST,
+                model: new FluidModel(),
+                initialConditions: new FluidInitialConditions(),
+                advancedConcepts: new AdvancedConcepts(),
+                materials: new ConvectiveHeatTransferMaterials(),
+                numerics: new FluidNumerics(
+                    relaxationFactor: new RelaxationFactor(),
+                    pressureReferenceValue: new DimensionalPressure(
+                        value: 0,
+                        unit: DimensionalPressure.UnitEnum.Pa
+                    ),
+                    residualControls: new ResidualControls(
+                        velocity: new Tolerance(),
+                        pressureRgh: new Tolerance(),
+                        turbulentKineticEnergy: new Tolerance(),
+                        omegaDissipationRate: new Tolerance(),
+                        temperature: new Tolerance()
+                    ),
+                    solvers: new FluidSolvers(),
+                    schemes: new Schemes(
+                        timeDifferentiation: new TimeDifferentiationSchemes(),
+                        gradient: new GradientSchemes(),
+                        divergence: new DivergenceSchemes(),
+                        laplacian: new LaplacianSchemes(),
+                        interpolation: new InterpolationSchemes(),
+                        surfaceNormalGradient: new SurfaceNormalGradientSchemes()
+                    )
+                ),
+                boundaryConditions: new List<OneOfConvectiveHeatTransferBoundaryConditions>() {
+                new VelocityInletBC(
+                    name: "Velocity inlet 1",
+                    velocity: new FixedValueVBC(
+                        value: new DimensionalVectorFunctionSpeed(
+                            value: new ComponentVectorFunction(
+                                x: new ConstantFunction(value: 0),
+                                y: new ConstantFunction(value: (decimal) -0.001),
+                                z: new ConstantFunction(value: 0)
+                            )
+                        )
+                    ),
+                    temperature: new FixedValueTBC(
+                        value: new DimensionalFunctionTemperature(
+                            value: new ConstantFunction(value: (decimal) 19.85),
+                            unit: DimensionalFunctionTemperature.UnitEnum.C
+                        )
+                    ),
+                    topologicalReference: new TopologicalReference(
+                        entities: new List<string>() { bc1Entity }
+                    )
+                ),
+                new PressureOutletBC(
+                    name: "Pressure outlet 2",
+                    gaugePressureRgh: new FixedValuePBC(
+                        value: new DimensionalFunctionPressure(
+                            value: new ConstantFunction(value: 0),
+                            unit: DimensionalFunctionPressure.UnitEnum.Pa
+                        )
+                    ),
+                    topologicalReference: new TopologicalReference(
+                        entities: new List<string>() { bc2Entity }
+                    )
+                ),
+                new PressureOutletBC(
+                    name: "Pressure outlet 3",
+                    gaugePressureRgh: new FixedValuePBC(
+                        value: new DimensionalFunctionPressure(
+                            value: new ConstantFunction(value: 0),
+                            unit: DimensionalFunctionPressure.UnitEnum.Pa
+                        )
+                    ),
+                    topologicalReference: new TopologicalReference(
+                        entities: new List<string>() { bc3Entity }
+                    )
+                ),
+                new WallBC(
+                    name: "Wall 4",
+                    velocity: new NoSlipVBC(
+                        turbulenceWall: NoSlipVBC.TurbulenceWallEnum.WALLFUNCTION
+                    ),
+                    temperature: new FixedValueTBC(
+                        value: new DimensionalFunctionTemperature(
+                            value: new ConstantFunction(value: 285),
+                            unit: DimensionalFunctionTemperature.UnitEnum.C
+                        )
+                    ),
+                    topologicalReference: new TopologicalReference(
+                        entities: new List<string>() { bc4Entity }
+                    )
+                )
+                },
+                simulationControl: new FluidSimulationControl(
+                    endTime: new DimensionalTime(
+                        value: 100,
+                        unit: DimensionalTime.UnitEnum.S
+                    ),
+                    deltaT: new DimensionalTime(
+                        value: 1,
+                        unit: DimensionalTime.UnitEnum.S
+                    ),
+                    maxRunTime: new DimensionalTime(
+                        value: 10000,
+                        unit: DimensionalTime.UnitEnum.S
+                    ),
+                    writeControl: new TimeStepWriteControl(
+                        writeInterval: 100
+                    ),
+                    decomposeAlgorithm: new ScotchDecomposeAlgorithm()
+                ),
+                resultControl: new FluidResultControls(
+                    forcesMoments: new List<OneOfFluidResultControlsForcesMoments>(),
+                    surfaceData: new List<OneOfFluidResultControlsSurfaceData>() {
+                    new AreaAverageResultControl(
+                        name: "Area average 1",
+                        writeControl: new TimeStepWriteControl(writeInterval: 1),
+                        topologicalReference: new TopologicalReference(
+                            entities: new List<string>() { bc4Entity }
+                        )
+                    )
+                    },
+                    probePoints: new List<ProbePointsResultControl>() {
+                    new ProbePointsResultControl(
+                        name: "Probe point 1",
+                        writeControl: new TimeStepWriteControl(writeInterval: 1),
+                        geometryPrimitiveUuids: new List<Guid?>() { geometryPrimitiveId }
+                    )
+                    }
+                )
+            );
+        }
+        public static string getSingleEntityName(GeometriesApi geometryApi, string projectId, Guid geometryId,
+                                       List<string> values = null)
+        {
+            var entities = geometryApi.GetGeometryMappings(
+                projectId: projectId,
+                geometryId: geometryId,
+                attributes: new List<string> { "SDL/TYSA_NAME" },
+                values: values
+            ).Embedded;
+            if (entities.Count == 1)
+            {
+                return entities[0].Name;
+            }
+            else
+            {
+                throw new Exception("Unexpected number of entities returned");
+            }
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("A41F6369-ABF5-4DD8-8244-62D3810C6F15"); }
+        }
+    }
+}
--- SimGH/SetEnvironment.cs
+++ SimGH/SetEnvironment.cs
@@ -15,6 +15,7 @@ using System.IO;
 using System.Threading;
 using static System.Net.WebRequestMethods;
 using SimGH.Properties;
+using Grasshopper.Kernel.Types.Transforms;
 
 
 namespace SimGH
@@ -44,7 +45,7 @@ namespace SimGH
             pManager.AddTextParameter("Description", "D", "Project Description", GH_ParamAccess.item);
             pManager.AddTextParameter("APIKey", "K", "Your API Key", GH_ParamAccess.item);
             pManager.AddTextParameter("FilePath", "F", "Geometry File Path", GH_ParamAccess.item);
-            pManager.AddIntegerParameter("MaterialCount", "M", "Number of materials to assign", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Upload", "U", "Set true to create project and upload geometry", GH_ParamAccess.item);
             pManager[1].Optional = true;
         }
 
@@ -53,9 +54,16 @@ namespace SimGH
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            
+            pManager.AddTextParameter("ProjectID", "P", "Project ID to access project", GH_ParamAccess.item);
+            pManager.AddGenericParameter("GeometryID", "G", "Geometry ID to access geometry", GH_ParamAccess.item);
+            pManager.AddGenericParameter("GeometryAPI", "GA", "Geometry API", GH_ParamAccess.item);
+            pManager.AddGenericParameter("SimulationAPI", "SA", "Simulation API", GH_ParamAccess.item);
         }
 
+        string projectId = default;
+        Guid geometryId = default;
+        GeometriesApi geometryApi = new GeometriesApi();
+        SimulationsApi simulationApi = new SimulationsApi();
         /// <summary>
         /// This is the method that actually does the work.
         /// </summary>
@@ -67,120 +75,100 @@ namespace SimGH
             string projectDescription = "description";
             string apiKey = default;
             string filePath = default;
-            int materialCount = 1;
+            bool upload = false;
 
             DA.GetData(0, ref projectName);
             DA.GetData(1, ref projectDescription);
             DA.GetData(2, ref apiKey);
             DA.GetData(3, ref filePath);
-            DA.GetData(4, ref materialCount);
+            DA.GetData(4, ref upload);
 
-            // API client configuration
-            var API_KEY_HEADER = "X-API-KEY";
-            //var API_KEY = Environment.GetEnvironmentVariable("SIMSCALE_API_KEY");
-            //var API_URL = Environment.GetEnvironmentVariable("SIMSCALE_API_URL");
-            var API_URL = "https://api.simscale.com";
-            var API_KEY = apiKey;
 
-            Configuration config = new Configuration();
-            config.BasePath = API_URL + "/v0";
-            config.ApiKey.Add(API_KEY_HEADER, API_KEY);
-
-            // API clients
-            var restClient = new RestClient();
-            var projectApi = new ProjectsApi(config);
-            var storageApi = new StorageApi(config);
-            var geometryImportApi = new GeometryImportsApi(config);
-            var geometryApi = new GeometriesApi(config);
-            var meshOperationApi = new MeshOperationsApi(config);
-            var simulationApi = new SimulationsApi(config);
-            var simulationRunApi = new SimulationRunsApi(config);
-            var tableImportApi = new TableImportsApi(config);
-            var reportsApi = new ReportsApi(config);
-            var materialsApi = new MaterialsApi(config);
-
-            HashSet<SimScale.Sdk.Model.Status> terminalStatuses = new HashSet<SimScale.Sdk.Model.Status> { SimScale.Sdk.Model.Status.FINISHED, SimScale.Sdk.Model.Status.CANCELED, SimScale.Sdk.Model.Status.FAILED };
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Restart();
-            var failedTries = 0;
-
-            // Create project
-            var project = new Project(
-                name: projectName,
-                description: projectDescription,
-                measurementSystem: Project.MeasurementSystemEnum.SI
-            );
-            project = projectApi.CreateProject(project);
-            var projectId = project.ProjectId;
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "projectId: " + projectId);
-
-            // Upload CAD
-            var storage = storageApi.CreateStorage();
-            var storageId = storage.StorageId;
-            var uploadRequest = new RestRequest(storage.Url, Method.PUT);
-            uploadRequest.AddParameter("application/octet-stream", System.IO.File.ReadAllBytes(@filePath), ParameterType.RequestBody);
-            restClient.Execute(uploadRequest);
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "storageId: " + storageId);
-
-            // Import CAD
-            var geometryImportRequest = new GeometryImportRequest(
-                name: "a",
-                location: new GeometryImportRequestLocation(storageId),
-                format: GeometryImportRequest.FormatEnum.RHINOCEROS,
-                inputUnit: GeometryUnit.M,
-                options: new GeometryImportRequestOptions(facetSplit: false, sewing: false, improve: true, optimizeForLBMSolver: false)
-            );
-            var geometryImport = geometryImportApi.ImportGeometry(projectId, geometryImportRequest);
-            var geometryImportId = geometryImport.GeometryImportId.Value;
-            stopWatch.Restart();
-            failedTries = 0;
-            while (!terminalStatuses.Contains(geometryImport.Status))
+            if (upload)
             {
-                // adjust timeout for larger geometries
-                if (stopWatch.Elapsed.TotalSeconds > 900)
+                // API client configuration
+                var API_KEY_HEADER = "X-API-KEY";
+                //var API_KEY = Environment.GetEnvironmentVariable("SIMSCALE_API_KEY");
+                //var API_URL = Environment.GetEnvironmentVariable("SIMSCALE_API_URL");
+                var API_URL = "https://api.simscale.com";
+                var API_KEY = apiKey;
+
+                Configuration config = new Configuration();
+                config.BasePath = API_URL + "/v0";
+                config.ApiKey.Add(API_KEY_HEADER, API_KEY);
+
+                // API clients
+                var restClient = new RestClient();
+                var projectApi = new ProjectsApi(config);
+                var storageApi = new StorageApi(config);
+                var geometryImportApi = new GeometryImportsApi(config);
+                geometryApi = new GeometriesApi(config);
+                var meshOperationApi = new MeshOperationsApi(config);
+                simulationApi = new SimulationsApi(config);
+                var simulationRunApi = new SimulationRunsApi(config);
+                var tableImportApi = new TableImportsApi(config);
+                var reportsApi = new ReportsApi(config);
+                var materialsApi = new MaterialsApi(config);
+
+                HashSet<SimScale.Sdk.Model.Status> terminalStatuses = new HashSet<SimScale.Sdk.Model.Status> { SimScale.Sdk.Model.Status.FINISHED, SimScale.Sdk.Model.Status.CANCELED, SimScale.Sdk.Model.Status.FAILED };
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Restart();
+                var failedTries = 0;
+
+                // Create project
+                var project = new Project(
+                    name: projectName,
+                    description: projectDescription,
+                    measurementSystem: Project.MeasurementSystemEnum.SI
+                );
+                project = projectApi.CreateProject(project);
+                projectId = project.ProjectId;
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "projectId: " + projectId);
+
+                // Upload CAD
+                var storage = storageApi.CreateStorage();
+                var storageId = storage.StorageId;
+                var uploadRequest = new RestRequest(storage.Url, Method.PUT);
+                uploadRequest.AddParameter("application/octet-stream", System.IO.File.ReadAllBytes(@filePath), ParameterType.RequestBody);
+                restClient.Execute(uploadRequest);
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "storageId: " + storageId);
+
+                // Import CAD
+                var geometryImportRequest = new GeometryImportRequest(
+                    name: "a",
+                    location: new GeometryImportRequestLocation(storageId),
+                    format: GeometryImportRequest.FormatEnum.RHINOCEROS,
+                    inputUnit: GeometryUnit.M,
+                    options: new GeometryImportRequestOptions(facetSplit: false, sewing: false, improve: true, optimizeForLBMSolver: false)
+                );
+                var geometryImport = geometryImportApi.ImportGeometry(projectId, geometryImportRequest);
+                var geometryImportId = geometryImport.GeometryImportId.Value;
+                stopWatch.Restart();
+                failedTries = 0;
+                while (!terminalStatuses.Contains(geometryImport.Status))
                 {
-                    throw new TimeoutException();
+                    // adjust timeout for larger geometries
+                    if (stopWatch.Elapsed.TotalSeconds > 900)
+                    {
+                        throw new TimeoutException();
+                    }
+                    Thread.Sleep(10000);
+                    geometryImport = geometryImportApi.GetGeometryImport(projectId, geometryImportId) ??
+                        (++failedTries > 5 ? throw new Exception("HTTP request failed too many times.") : geometryImport);
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "Geometry import status: " + geometryImport?.Status);
                 }
-                Thread.Sleep(10000);
-                geometryImport = geometryImportApi.GetGeometryImport(projectId, geometryImportId) ??
-                    (++failedTries > 5 ? throw new Exception("HTTP request failed too many times.") : geometryImport);
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "Geometry import status: " + geometryImport?.Status);
+                geometryId = geometryImport.GeometryId.Value;
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "geometryId: " + geometryId);
             }
-            var geometryId = geometryImport.GeometryId.Value;
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "geometryId: " + geometryId);
 
-            // Read geometry information and update with the deserialized model
-            var geometry = geometryApi.GetGeometry(projectId, geometryId);
-            geometryApi.UpdateGeometry(projectId, geometryId, geometry);
+            DA.SetData(0, projectId);
+            DA.SetData(1, geometryId);
+            DA.SetData(2, geometryApi);
+            DA.SetData(3, simulationApi);
 
-            //Get geometry mappings
-            for (int m = 0; m < materialCount; m++)
-            {
-                var materialEntity = getSingleEntityName(
-                   geometryApi,
-                   projectId,
-                   geometryId,
-                   values: new List<string> { "block1" } );
-            }
         }
-        public static string getSingleEntityName(GeometriesApi geometryApi, string projectId, Guid geometryId,
-                                               List<string> values = null)
-        {
-            var entities = geometryApi.GetGeometryMappings(
-                projectId: projectId,
-                geometryId: geometryId,
-                attributes: new List<string> { "SDL/TYSA_NAME" },
-                values: values
-            ).Embedded;
-            if (entities.Count == 1)
-            {
-                return entities[0].Name;
-            }
-            else
-            {
-                throw new Exception("Unexpected number of entities returned");
-            }
-        }
+
+
 
         /// <summary>
         /// Provides an Icon for every component that will be visible in the User Interface.
