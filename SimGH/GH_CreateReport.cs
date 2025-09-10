cfe7f2afebc213c83aaddbfd4722384eb00689bd
yyen <yyen@kglakademi.dk>
2025-09-10 16:20:10 +0200

    add simscale project info class to pass the data between components

--- SimGH/GH_CreateReport.cs
+++ SimGH/GH_CreateReport.cs
@@ -40,78 +40,78 @@ namespace SimGH
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // Create report
-            var scalarField = new ScalarField(
-                fieldName: "Temperature",
-                dataType: DataType.CELL
-            );
-            var reportRequest = new ReportRequest(
-                name: "Report 1",
-                description: "Simulation report",
-                resultIds: new List<Guid?> {
-                solutionInfo.ResultId
-                },
-                reportProperties: new ScreenshotReportProperties(
-                    modelSettings: new ModelSettings(
-                        scalarField: scalarField
-                    ),
-                    filters: new Filters(
-                        cuttingPlanes: new List<CuttingPlane> {
-                        new CuttingPlane(
-                            name: "velocity-plane",
-                            scalarField: scalarField,
-                            center: new Vector3D(x: 0.05m, y: 0m, z: 0m),
-                            normal: new Vector3D(x: 1m, y: 0m, z: 0m),
-                            opacity: 1,
-                            clipping: true,
-                            renderMode: RenderMode.SURFACES
-                        )
-                        }
-                    ),
-                    cameraSettings: new TopViewPredefinedCameraSettings(
-                        projectionType: ProjectionType.ORTHOGONAL,
-                        directionSpecifier: TopViewPredefinedCameraSettings.DirectionSpecifierEnum.XNEGATIVE
-                    ),
-                    outputSettings: new ScreenshotOutputSettings(
-                        name: "Output 1",
-                        format: ScreenshotOutputSettings.FormatEnum.PNG,
-                        resolution: new ResolutionInfo(x: 1440, y: 1080),
-                        frameIndex: 1,
-                        showLegend: true,
-                        showCube: false
-                    )
-                )
-            );
+        //    // Create report
+        //    var scalarField = new ScalarField(
+        //        fieldName: "Temperature",
+        //        dataType: DataType.CELL
+        //    );
+        //    var reportRequest = new ReportRequest(
+        //        name: "Report 1",
+        //        description: "Simulation report",
+        //        resultIds: new List<Guid?> {
+        //        solutionInfo.ResultId
+        //        },
+        //        reportProperties: new ScreenshotReportProperties(
+        //            modelSettings: new ModelSettings(
+        //                scalarField: scalarField
+        //            ),
+        //            filters: new Filters(
+        //                cuttingPlanes: new List<CuttingPlane> {
+        //                new CuttingPlane(
+        //                    name: "velocity-plane",
+        //                    scalarField: scalarField,
+        //                    center: new Vector3D(x: 0.05m, y: 0m, z: 0m),
+        //                    normal: new Vector3D(x: 1m, y: 0m, z: 0m),
+        //                    opacity: 1,
+        //                    clipping: true,
+        //                    renderMode: RenderMode.SURFACES
+        //                )
+        //                }
+        //            ),
+        //            cameraSettings: new TopViewPredefinedCameraSettings(
+        //                projectionType: ProjectionType.ORTHOGONAL,
+        //                directionSpecifier: TopViewPredefinedCameraSettings.DirectionSpecifierEnum.XNEGATIVE
+        //            ),
+        //            outputSettings: new ScreenshotOutputSettings(
+        //                name: "Output 1",
+        //                format: ScreenshotOutputSettings.FormatEnum.PNG,
+        //                resolution: new ResolutionInfo(x: 1440, y: 1080),
+        //                frameIndex: 1,
+        //                showLegend: true,
+        //                showCube: false
+        //            )
+        //        )
+        //    );
 
-            // Creating report
-            Console.WriteLine("Creating report...");
-            var createReportResponse = reportsApi.CreateReport(projectId, reportRequest);
-            var reportId = createReportResponse.ReportId;
+        //    // Creating report
+        //    Console.WriteLine("Creating report...");
+        //    var createReportResponse = reportsApi.CreateReport(projectId, reportRequest);
+        //    var reportId = createReportResponse.ReportId;
 
-            // Start report job
-            Console.WriteLine("Starting report with ID: " + reportId);
-            reportsApi.StartReportJob(projectId, reportId);
+        //    // Start report job
+        //    Console.WriteLine("Starting report with ID: " + reportId);
+        //    reportsApi.StartReportJob(projectId, reportId);
 
-            HashSet<ReportResponse.StatusEnum> terminalReportStatuses = new HashSet<ReportResponse.StatusEnum> {
-            ReportResponse.StatusEnum.FINISHED,
-                ReportResponse.StatusEnum.CANCELED,
-                ReportResponse.StatusEnum.FAILED
-        };
-            var report = reportsApi.GetReport(projectId, reportId);
+        //    HashSet<ReportResponse.StatusEnum> terminalReportStatuses = new HashSet<ReportResponse.StatusEnum> {
+        //    ReportResponse.StatusEnum.FINISHED,
+        //        ReportResponse.StatusEnum.CANCELED,
+        //        ReportResponse.StatusEnum.FAILED
+        //};
+        //    var report = reportsApi.GetReport(projectId, reportId);
 
-            while (!terminalReportStatuses.Contains(report.Status))
-            {
-                Thread.Sleep(30000);
-                report = reportsApi.GetReport(projectId, reportId);
-                Console.WriteLine("Report generation status: " + report.Status);
-            }
+        //    while (!terminalReportStatuses.Contains(report.Status))
+        //    {
+        //        Thread.Sleep(30000);
+        //        report = reportsApi.GetReport(projectId, reportId);
+        //        Console.WriteLine("Report generation status: " + report.Status);
+        //    }
 
-            using (var client = new WebClient())
-            {
-                Console.WriteLine("Downloading file from: " + report.Download.Url);
-                client.Headers.Add(API_KEY_HEADER, API_KEY);
-                client.DownloadFile(report.Download.Url, "report." + report.Download.Format);
-            }
+        //    using (var client = new WebClient())
+        //    {
+        //        Console.WriteLine("Downloading file from: " + report.Download.Url);
+        //        client.Headers.Add(API_KEY_HEADER, API_KEY);
+        //        client.DownloadFile(report.Download.Url, "report." + report.Download.Format);
+        //    }
         }
 
         /// <summary>
---
+++ SimGH/SimProjectInfo.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using SimScale.Sdk.Api;
+using SimScale.Sdk.Client;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimGH
+{
+    public class SimProjectInfo
+    {
+        public SimProjectInfo() { }
+
+        public SimulationsApi SimulationApi { get; private set; }
+        public Guid? SimulationId { get; private set; }
+        public GeometriesApi GeometryApi  { get; private set; }
+        public MeshOperationsApi MeshOperationApi { get; private set; }
+        public MaterialsApi MaterialsApi { get; private set; }
+        public SimulationRunsApi SimulationRunApi { get; private set; }
+        public TableImportsApi TableImportApi { get; private set; }
+        public ReportsApi ReportsApi { get; private set; }
+        public string ProjectName {  get; private set; }
+        public Configuration Configuration { get; private set; }
+
+        public void SetSimulationApi(SimulationsApi simulationsApi) => SimulationApi = simulationsApi;
+        public void SetSimulationId(Guid? guid) => SimulationId = guid;
+        public void SetGeometriesApi(GeometriesApi geometryApi) => GeometryApi = geometryApi;
+        public void SetMeshOperationsApi(MeshOperationsApi meshOperationApi) => MeshOperationApi = meshOperationApi;
+        public void SetMaterialsApi(MaterialsApi materialsApi) => MaterialsApi = materialsApi;
+        public void SetSimulationRunsApi(SimulationRunsApi simulationRunApi) => SimulationRunApi = simulationRunApi;
+        public void SetTableImportApi(TableImportsApi tableImportApi) => TableImportApi = tableImportApi;
+        public void SetReportsApi(ReportsApi reportsApi) => ReportsApi = reportsApi;
+        public void SetProjectName(string projectName) => ProjectName = projectName;
+        public void SetConfiguration(Configuration configuration) => Configuration = configuration;
+
+    }
+}
